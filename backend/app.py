b82b484b1c5a94dff69db0f298c93b9753ceb131
David Bao <davidbao@berkeley.edu>
2020-07-10 22:00:54 -0700

    generate playlist from seed tracks functionality

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from getUserTops import getTops
 from createPlaylist import generate
 from spot_auth import user_id
+from getRecs import recommendTracks
 
 topTracks, topArtists = getTops('short_term')
 
@@ -8,11 +9,18 @@ to_playlist = []
 
 print("My Top Tracks")
 for i, item in enumerate(topTracks):
-    print(i+1, item['name'], '//', item['artists'][0]['name'])
-    to_playlist.append(item['id'])
+    # print(i+1, item['name'], '//', item['artists'][0]['name'])
 
-print("\nMy Top Artists")
-for i, item in enumerate(topArtists):
-    print(i+1, item['name'])
+    if i < 5:
+        to_playlist.append('spotify:track:' + item['id'])
 
-recs_list = generate("API Playlist Generation Test", to_playlist)
+# print("\nMy Top Artists")
+# for i, item in enumerate(topArtists):
+#     print(i+1, item['name'])
+
+results = recommendTracks(tracks=to_playlist)
+# for r in recs['tracks']:
+#     print("recommendation:", r['name'], "by", r['artists'][0]['name'])
+recs = [track['id'] for track in results['tracks']]
+
+recs_list = generate("API Playlist Recommendation Test", recs)
--- backend/spot_auth.py
+++ backend/spot_auth.py
@@ -1,8 +1,8 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-SPOTIPY_CLIENT_ID="no"
-SPOTIPY_CLIENT_SECRET="no"
+SPOTIPY_CLIENT_ID="7c37dc9f80124866a7549d6128f0b9c2"
+SPOTIPY_CLIENT_SECRET="8121a1f64d2947e09c8ac711d396f15a"
 SPOTIPY_REDIRECT_URI="http://localhost:8888/callback/"
 
 CACHE = ".cache-" + "test"
@@ -10,4 +10,4 @@ scope = 'playlist-modify-public user-read-email user-top-read'
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI, scope=scope, cache_path=CACHE))
 # user_id = sp.me()['id']
-user_id = "nuh uh"
+user_id = 1263126600
