5f9fff6d5df9c53c44abb2be09eb7b99335bda9a
alden <aldenliu98@berkeley.edu>
2020-07-13 00:53:47 -0700

    hh

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,15 @@ import './App.css';
 import SpotifyWebApi from "spotify-web-api-js";
 import TopBar from './components/TopBar';
 import LoginButton from './components/loginButton';
+
+import {
+  Row,
+  Col,
+  Container
+} from "reactstrap";
+
 const spot = new SpotifyWebApi();
+
 
 class App extends Component {
   constructor(){
@@ -148,8 +156,16 @@ class App extends Component {
           <div>
             { !this.state.loggedIn &&
               <div>
-                <p>Do you even know a brother?</p>
-                <a href='http://localhost:8888' > <LoginButton msg="Log In To Spotify Now"/> </a>
+                <h1>Do you even know a brother?</h1>
+                <Container>
+                  <Row className="justify-content-md-center">
+                    <Col lg="6">
+                      <p>What the fuck did you just fucking say about me, you little bitch? I'll have you know I graduated top of my class in the Navy Seals, and I've been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I'm the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You're fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that's just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little "clever" comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn't, you didn't, and now you're paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You're fucking dead, kiddo.</p>
+                    </Col>
+                  </Row>
+                </Container>
+                <br />
+                <a href='http://localhost:8888' > <LoginButton msg="Log In To Spotify Now, CMON!!!!"/> </a>
               </div>
             }
           </div>
--- client/src/components/TopBar.js
+++ client/src/components/TopBar.js
@@ -13,7 +13,7 @@ import '../styles/TopBar.css';
     render() {
       return (
         <>
-          <Navbar color-on-scroll="100" className="navbar-transparent tuneinnav" expand="lg">
+          <Navbar color-on-scroll="100" className="navbar-transparent tuneinnav sticky-top" expand="lg">
             <NavbarBrand href="#" onClick={e => e.preventDefault()}>
                 <h3 id="content" style={{margin:0}}>TuneIn</h3>
             </NavbarBrand>
